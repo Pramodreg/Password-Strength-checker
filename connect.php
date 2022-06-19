5e1f5f5760bef4f3dd14d578b8034f12ad25d6e0
Pramodreg <reedmii90@gmail.com>
2022-06-19 10:26:09 +0545

    php code added

--- connect.php
+++ connect.php
@@ -1,19 +1,21 @@
 <?php
-$Uname = $_POST['UserName'];
-$Pname = $_POST['UserName'];
-$Cpname = $_POST['UserName'];
+// $Uname = $_POST['UserName'];
+// $Pname = $_POST['Passwordd'];
+// $Cpname = $_POST['C-password'];
 
-$conn = new mysqli('localhost','root','','passwordstrength');
-	if($conn->connect_error){
-		echo "$conn->connect_error";
-		die("Connection Failed : ". $conn->connect_error);
-	} else {
-		$stmt = $conn->prepare("insert into registration(UserName, Passwordd, C-password) values(?, ?, ?)");
-		$stmt->bind_param("sssssi", $Userame, $Passwordd, $cpassword);
-		$execval = $stmt->execute();
-		echo $execval;
-		echo "Registration successfully...";
-		$stmt->close();
-		$conn->close();
-	}
-?>
+// $conn = new mysqli('localhost','root','','passwordstrength');
+// 	if($conn->connect_error){
+// 		echo "$conn->connect_error";
+// 		die("Connection Failed : ". $conn->connect_error);
+// 	} else {
+
+
+// 		$stmt = $conn->prepare("insert into registration(UserName, Passwordd, C-password) values(?, ?, ?)");
+// 		$stmt->bind_param("sssssi", $Userame, $Passwordd, $cpassword);
+// 		$execval = $stmt->execute();
+// 		echo $execval;
+// 		echo "Registration successfully...";
+// 		$stmt->close();
+// 		$conn->close();
+// 	}
+?> -->
