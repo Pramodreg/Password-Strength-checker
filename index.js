7212e9b48d2761b6deda8949bc1b8495c6543248
subash <imsuubash@gmail.com>
2022-06-22 20:38:05 +0545

    css milau hai

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
-import express from 'express'
-import mysql from 'mysql'
-import bodyParser from "body-parser";
+const express = require("express");
+// const mysql = require("mysql");
 const app = express();
-app.use(bodyParser.json());
-const connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'system'
-});
-connection.connect((err)=>{
-    if(err)throw err;
-    console.log("Connected..");
-    // insertData('Pramod', '12344', '12344');
+const path = require("path");
+const router = express.Router();
+//add the router
+app.use("/", router);
 
-     getData();   // getting all the data from table
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "system",
 });
-function getData(){
-    let sql = 'SELECT * FROM register';
-    connection.query(sql,(err, result)=>{
-        if(err) throw err;
-        console.log(result);
-    });
-    }
-    app.get('/',function(req,res) {
-        res.render('index');
-      });
-app.listen(5000, () => {
-  console.log(`App listening at http://localhost:5000`);
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected..");
+  // insertData('Pramod', '12344', '12344');
+
+  getData(); // getting all the data from table
+});
+function getData() {
+  let sql = "SELECT * FROM register";
+  connection.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  });
+}
+
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+  //__dirname : It will resolve to your project folder.
+});
+
+router.get("/login", function (req, res) {
+  res.sendFile(path.join(__dirname + "/login.html"));
+});
+
+router.get("/dashboard", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dashboard.html"));
+});
+
+app.listen(process.env.port || 3000, () => {
+  console.log("Running at Port 3000");
 });
